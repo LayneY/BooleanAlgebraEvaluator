3d9a3f8ab4f5dab079eb41fe9fb47d5b7aab76c9
Layne Yarbrough <layne.yarbrough@student.allenisd.org>
2024-01-25 20:41:40 -0600

    recoded whole thing to include more operators and more variables

--- main.swift
+++ main.swift
@@ -1,134 +1,229 @@
 import Foundation
 
-enum Token {
-    case variable(String)
-    case operatorToken(String)
+enum BooleanAlgebra {
+    enum TokenError: Error {
+        case noneFound
+    }
+    
+    case variable(value: String)
+    case operatorToken(value: String)
+    case trueVal
+    case falseVal
     case openParenthesis
-    case closeParenthesis
+    case closedParenthesis
+    case empty
+
+    static func nextToken(from line: Substring) throws -> (token: BooleanAlgebra, text: Substring, characters: Int) {
+        if let match = try #/(T)/#.prefixMatch(in: line) {
+            return (token: .trueVal, text: match.1, characters: match.0.count)
+        }else if let match = try #/(F)/#.prefixMatch(in: line) {
+            return (token: .falseVal, text: match.1, characters: match.0.count)
+        }else if let match = try #/([A-Za-z]+)/#.prefixMatch(in: line) {
+            return (token: .variable(value: String(match.1)), text: match.1, characters: match.0.count)
+        }else if let match = try #/([&|0>=!])/#.prefixMatch(in:line) {
+            return (token: .operatorToken(value: String(match.1)), text: match.1, characters: match.0.count)
+        }else{
+            throw TokenError.noneFound
+        }
+    }
 }
 
-class BooleanEvaluator {
-    private var tokens: [Token] = []
-    private var currentIndex: Int = 2
-    
-    init(equation: String) {
-        self.tokens = tokenize(equation)
-    }
+struct TokenSource {
+    let token: BooleanAlgebra
+    let text: String
+    let lineIndex: Int
+    let columnIndex: Int
 
-    private func tokenize(_ equation: String) -> [Token] {
-        var tokens: [Token] = []
-        var currentIndex = equation.startIndex
-        
-        while currentIndex < equation.endIndex {
-            let currentChar = equation[currentIndex].lowercased()
-            
-            switch currentChar {
-            case "a"..."z":
-                // Recognize variables
-                var variable = ""
-                while currentIndex < equation.endIndex, case "a"..."z" = equation[currentIndex] {
-                    variable.append(equation[currentIndex])
-                    currentIndex = equation.index(after: currentIndex)
-                }
-                tokens.append(.variable(variable))
-                
-            case "&", "|", "!", "and", "or", "not", "equal", "notequal":
-                // Recognize operators
-                tokens.append(.operatorToken(String(currentChar)))
-                currentIndex = equation.index(after: currentIndex)
-                
-            case "(":
-                // Recognize opening parenthesis
-                tokens.append(.openParenthesis)
-                currentIndex = equation.index(after: currentIndex)
-                
-            case ")":
-                // Recognize closing parenthesis
-                tokens.append(.closeParenthesis)
-                currentIndex = equation.index(after: currentIndex)
-                
-            case " ":
-                // Skip whitespace
-                currentIndex = equation.index(after: currentIndex)
-                
-            default:
-                // Ignore unrecognized characters
-                currentIndex = equation.index(after: currentIndex)
-            }
+    static func lex(line: String) throws -> [BooleanAlgebra] {
+        var workingLine = Substring(line)
+        var tokens: [BooleanAlgebra] = []
+        while workingLine.count > 0 {
+            let (token, _, characters) = try BooleanAlgebra.nextToken(from: workingLine)
+            workingLine = workingLine.dropFirst(characters)
+            tokens.append(token)
         }
-        
         return tokens
     }
+}
 
-    func evaluate(values: [Bool]) -> Int {
-        while currentIndex < tokens.count {
-            if case .variable = tokens[currentIndex - 2] {
-                if case .variable = tokens[currentIndex] {
-                    switch tokens[currentIndex-1] {
-                    case .operatorToken("&"):
-                        if values[0] && values[1] {
-                            return 1
-                        }else{
-                            return 0
-                        }
-                    case .operatorToken("|"):
-                        if values[0] || values[1] {
-                            return 1
-                        }else{
-                            return 0
-                        }
-                    case .operatorToken("0"):
-                        if (values[0] == true && values[1] == false) || (values[0] == false && values[1] == true) {
-                            return 1
-                        }else{
-                            return 0
-                        }
-                    case .operatorToken("="):
-                        if values[0] == values[1] {
-                            return 1
-                        }else{
-                            return 0
-                        }
-                    default:
-                        return 0
-                    }
-                }
-            }
+class Stack {
+    public var items: [BooleanAlgebra] = Array()
+
+    public var topIndex = 0
+
+    init() {
+        for _ in 0..<10 {
+            items.append(.empty)
         }
-        return 0
+    }
+    
+    func push(_ item: BooleanAlgebra) {
+        items[topIndex] = item
+        topIndex += 1
     }
 
-    func createTruthTable() {
-        var variables: [String] = []
+    func pop() -> BooleanAlgebra {
+        topIndex -= 1
+        return items[topIndex]
+    }
+    
+    func peek() -> BooleanAlgebra {
+        return items[topIndex - 1]
+    }
+
+    func isEmpty() -> Bool {
+        return topIndex == 0
+    }
+
+    func getCount() -> Int {
+        return topIndex
+    }
+
+    func copy() -> Stack {
+        let new = Stack()
+        new.topIndex = topIndex
+        new.items = items
+        return new
+    }
+
+    // func dump() {
+    //     var list = ""
+    //     for i in 0 ..< topIndex {
+    //         list += items[i] + " "
+    //     }
+    //     print(list)
+    // }
+
+}
+
+func main() {
+    print("Enter a boolean expression and press ENTER when done.")
+    print("Operators include: & (and), | (or), 0 (xor), = (equal), ! (not equal), > (imply), T (true), F (false).")
+    print("Variables must be at least one lowercase or uppercase letter, not including uppercase T and F.")
+    
+    let line = readLine()!
+    let stack = Stack()
+    var variables: [String] = []
+    do {
+        var tokens = try TokenSource.lex(line:line)
+        tokens.reverse()
         for token in tokens {
             switch token {
             case .variable(let value):
                 variables.append(value)
+                stack.push(token)
             default:
-                print()
+                stack.push(token)   
             }
         }
-
-        print(variables.joined(separator: "\t") + "\tOutput")
-
-        let combinationsCount = Int(pow(2.0, Double(variables.count)))
-        var combinations = [[Bool]]()
-
-        for i in 0..<combinationsCount {
-            let binaryString = String(i, radix: 2)
-            let binaryArray = binaryString.padding(toLength: variables.count, withPad: "0", startingAt: 0).compactMap { $0 == "1" }
-            combinations.append(binaryArray)
-        }
-
-        for combination in combinations {
-            let evaluationResult = evaluate(values: combination)
-            print(combination.map { $0 ? "1" : "0" }.joined(separator: "\t") + "\t" + String(evaluationResult))
-        }
+    } catch {
+        print("Failed bc: \(error)")
     }
 
+    switch stack.peek() {
+    case .operatorToken:
+        print("Expression ends in an operator.")
+    default:
+        variables.reverse()
+        createTruthTable(variables: variables, stack: stack)
+    }
 }
 
-let input = readLine()!
-let boolean = BooleanEvaluator(equation: input)
-boolean.createTruthTable()
+func evaluate(variables: [String], values: [Bool], stack: Stack) -> String {
+    while stack.getCount() > 1 {
+        let leftOp = stack.pop()
+        let op = stack.pop()
+        let rightOp = stack.pop()
+        var rightVal = false
+        var leftVal = false
 
+        if case .trueVal = rightOp {
+            rightVal = true
+        } else if case .variable(let val) = rightOp {
+            let varIndex = variables.firstIndex(of: val)!
+            rightVal = values[varIndex]
+        }
+
+        if case .trueVal = leftOp {
+            leftVal = true
+        } else if case .variable(let val) = leftOp {
+            let varIndex = variables.firstIndex(of: val)!
+            leftVal = values[varIndex]
+        }
+
+        if case .operatorToken(let val) = op {
+            switch val {
+            case "&"://and
+                if leftVal && rightVal {
+                    stack.push(.trueVal)
+                }else{
+                    stack.push(.falseVal)
+                }
+            case "|"://or
+                if leftVal || rightVal {
+                    stack.push(.trueVal)
+                }else{
+                    stack.push(.falseVal)
+                }
+            case "0", "!"://xor and not equal
+                if leftVal != rightVal {
+                    stack.push(.trueVal)
+                }else{
+                    stack.push(.falseVal)
+                }
+            case ">": //implies
+                if leftVal == false || (leftVal && rightVal) {
+                    stack.push(.trueVal)
+                }else{
+                    stack.push(.falseVal)
+                }
+            case "=": //equal
+                if leftVal == rightVal {
+                    stack.push(.trueVal)
+                }else{
+                    stack.push(.falseVal)
+                }
+            default:
+                return "no"
+            }
+        }else{
+            return "badop"
+        }
+    }
+    let final = stack.peek()
+    switch final {
+    case .trueVal:
+        return "T"
+    case .falseVal:
+        return "F"
+    default:
+        return "F"
+    }
+ 
+}
+
+func createTruthTable(variables: [String], stack: Stack) {
+    let variableCombinations = 1 << variables.count
+    
+    print(variables.joined(separator: ", "), " | Result")
+
+    for i in 0..<variableCombinations {
+        var variableValues: [String: Bool] = [:]
+        var rowValues: [Bool] = []
+
+        for (index, variable) in variables.enumerated() {
+            let bit = (i >> index) & 1
+            let value = (bit == 1)
+            variableValues[variable] = value
+            rowValues.append(value)
+        }
+
+        // Print truth table row
+        let row = variables.map { variableValues[$0]! ? "T" : "F" }.joined(separator: ", ")
+        let stackCopy = stack.copy()
+        let result = evaluate(variables: variables, values: rowValues, stack: stackCopy)
+        print(row, " | \(result)")
+    }
+}
+
+main()
